c91a17001f7b7facee6cf11a68c4a411642f3eaa
elgikore <elgienbertc@gmail.com>
2025-12-29 14:42:30 +1300

    forgot to add getter

--- monitors_list_gui.py
+++ monitors_list_gui.py
@@ -10,11 +10,14 @@ class MonitorList(Frame):
         label2.pack(padx=10, pady=3, anchor='w')
 
         monitors = get_monitors()
-        list_monitors_box = ttk.Combobox(root, values=monitors, state="readonly")
-        list_monitors_box.current(0)
-        list_monitors_box.pack(pady=1)
+        self.__list_monitors_box = ttk.Combobox(root, values=monitors, state="readonly")
+        self.__list_monitors_box.current(0)
+        self.__list_monitors_box.pack(pady=1)
 
         ok_button = ttk.Button(root, text="OK", command=command)
         ok_button.pack(pady=10)
+        
+    def get(self): 
+        return self.__list_monitors_box.get()
 
 
