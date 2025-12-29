28bf7724ceb829b8718ea2a98a7ce57a9b70a180
elgikore <elgienbertc@gmail.com>
2025-12-29 14:35:21 +1300

    added reusable monitor combobox

---
+++ monitors_list_gui.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk
+from utilities import get_monitors
+
+class MonitorList(Frame):
+    def __init__(self, root: Tk = None, command: callable = None, **kwargs):
+        super().__init__(root, **kwargs)
+        
+        label2 = ttk.Label(root, text="Monitor to place wallpaper:")
+        label2.pack(padx=10, pady=3, anchor='w')
+
+        monitors = get_monitors()
+        list_monitors_box = ttk.Combobox(root, values=monitors, state="readonly")
+        list_monitors_box.current(0)
+        list_monitors_box.pack(pady=1)
+
+        ok_button = ttk.Button(root, text="OK", command=command)
+        ok_button.pack(pady=10)
+
+
