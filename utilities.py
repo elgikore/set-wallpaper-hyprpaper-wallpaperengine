2d1fafb096e369a87fe10dd458dedc7aee08718d
elgikore <elgienbertc@gmail.com>
2025-12-29 14:36:26 +1300

    added resuable on_close method

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import os, subprocess
+from tkinter import Tk
 
 def force_floating_window_hyprland(window_name: str, is_enabled: bool = True) -> None:
     if os.getenv("XDG_CURRENT_DESKTOP") != "Hyprland": 
@@ -17,3 +18,8 @@ def get_monitors() -> list[str]:
                               capture_output=True, text=True)
     
     return sorted(monitors.stdout.rstrip().split('\n'))
+
+
+def on_close(root: Tk, window_title: str) -> None:
+    root.destroy()
+    force_floating_window_hyprland(window_title, is_enabled=False)
