f4ffa9323b1950975ccd999003fcebeea586792d
elgikore <elgienbertc@gmail.com>
2025-12-29 18:08:46 +1300

    made a wallpaper class instead of a namedtuple

--- set_wallpaper_wallpaperengine.py
+++ set_wallpaper_wallpaperengine.py
@@ -3,17 +3,26 @@ from tkinter import ttk, messagebox
 from pathlib import Path
 from utilities import get_monitors, force_floating_window_hyprland, on_close
 from monitors_list_gui import MonitorList
-from typing import NamedTuple
 import sys
 
 window_title = "Set wallpaper from Wallpaper Engine"
 wallpaperengine_path = Path("~/.steam/steam/steamapps/workshop/content/431960/").expanduser()
 
 
-class Wallpaper(NamedTuple):
-    id: str
-    path: Path
-    preview_pic: Path
+class Wallpaper():
+    def __init__(self, id: str, path: Path, preview_pic_path: Path):
+        self.id = id
+        self.path = path
+        self.preview_pic_path = preview_pic_path
+    
+    def __str__(self) -> str:
+        return self.id
+    
+    def __eq__(self, other_wallpaper):
+        if not isinstance(other_wallpaper, Wallpaper):
+            return NotImplemented
+        
+        return self.id == other_wallpaper.id
 
 
 def list_wallpaperengine_wallpapers():
@@ -34,15 +43,16 @@ root.geometry("500x240")
 root.protocol("WM_DELETE_WINDOW", lambda: on_close(root, window_title))
 force_floating_window_hyprland(window_title)
 
-# Frame to mix layouts, similar to JFrame
+# Frame for the main layout
 frame = Frame(root)
 frame.grid(row=0, column=0)
-wallpapers = list_wallpaperengine_wallpapers()
-wallpaper_ids = sorted([wallpaper.id for wallpaper in wallpapers])
+
+# First column
+wallpapers = sorted(list_wallpaperengine_wallpapers(), key=lambda x: x.id)
 
 wallpaper_list_frame = Frame(frame)
 scrollbar = Scrollbar(wallpaper_list_frame, orient="vertical")
-wallpaper_list = Listbox(wallpaper_list_frame, listvariable=Variable(value=wallpaper_ids), 
+wallpaper_list = Listbox(wallpaper_list_frame, listvariable=Variable(value=wallpapers), 
                         height=6, selectmode=SINGLE, yscrollcommand=scrollbar.set)
 scrollbar.config(command=wallpaper_list.yview)
 wallpaper_list.pack(side="left", fill="both", pady=5)
