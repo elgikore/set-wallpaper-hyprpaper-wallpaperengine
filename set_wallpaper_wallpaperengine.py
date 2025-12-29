537f438bbbe2aa736b3f87b0c01d6a275e26a654
elgikore <elgienbertc@gmail.com>
2025-12-29 18:24:55 +1300

    some organizing

--- set_wallpaper_wallpaperengine.py
+++ set_wallpaper_wallpaperengine.py
@@ -25,7 +25,7 @@ class Wallpaper():
         return self.id == other_wallpaper.id
 
 
-def list_wallpaperengine_wallpapers():
+def list_wallpaperengine_wallpapers() -> None:
     if wallpaperengine_path.exists():
         return [Wallpaper(pic_dir.name, pic_dir, pic_dir / "preview.jpg") 
                     for pic_dir in wallpaperengine_path.iterdir() 
@@ -39,26 +39,35 @@ def list_wallpaperengine_wallpapers():
 
 root = Tk()
 root.title(window_title)
-root.geometry("500x240")
+root.geometry("648x256")
 root.protocol("WM_DELETE_WINDOW", lambda: on_close(root, window_title))
 force_floating_window_hyprland(window_title)
 
-# Frame for the main layout
+# Frame for the first column
 frame = Frame(root)
 frame.grid(row=0, column=0)
 
 # First column
+# [Top] Listbox of wallpapers
 wallpapers = sorted(list_wallpaperengine_wallpapers(), key=lambda x: x.id)
-
 wallpaper_list_frame = Frame(frame)
+
 scrollbar = Scrollbar(wallpaper_list_frame, orient="vertical")
 wallpaper_list = Listbox(wallpaper_list_frame, listvariable=Variable(value=wallpapers), 
                         height=6, selectmode=SINGLE, yscrollcommand=scrollbar.set)
 scrollbar.config(command=wallpaper_list.yview)
+
 wallpaper_list.pack(side="left", fill="both", pady=5)
 scrollbar.pack(side="right", fill="y", pady=5)
 wallpaper_list_frame.pack(padx=10)
 
+# [Bottom] Monitor list
 monitor_list = MonitorList(frame)
 monitor_list.pack()
+
+
+# Second column
+preview_canvas = Canvas(root, width=426, height=240, bg="white")
+preview_canvas.grid(row=0, column=1, padx=10, pady=5)
+
 root.mainloop()
